df8bfac98216156d8a515f779ae125cf9db2baf2
Evaldo Santos <jesantosjr@protonmail.com>
2020-07-04 09:13:36 -0300

    react props

--- src/App.js
+++ src/App.js
@@ -4,26 +4,7 @@ function getTitle(title) {
   return title;
 }
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-function List() {
+const List = ({ list }) => {
   return list.map(function(item) {
     return (
       <div key={item.objectID}>
@@ -39,6 +20,25 @@ function List() {
 }
 
 function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   const handleChange = event => {
     console.log(event);
   };
@@ -49,7 +49,7 @@ function App() {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
   );
 }
