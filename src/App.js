390150c04dd049938c63925bf46a158cd3ac17c3
Evaldo Santos <jesantosjr@protonmail.com>
2020-07-06 21:58:40 -0300

    React Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -99,6 +99,8 @@ function App() {
   });
 
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
@@ -118,9 +120,12 @@ function App() {
   });
 
   React.useEffect(() => {
+    setIsLoading(true);
     getAsyncStories().then(result => {
       setStories(result.data.stories);
-    })
+      setIsLoading(false);
+    }).catch(() => setIsError(true));
+    
   }, [])
 
   return (
@@ -135,7 +140,9 @@ function App() {
         <strong>Search</strong>
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong ...</p>}
+      
+      { isLoading ? <p>Loading ...</p> : <List list={searchedStories} onRemoveItem={handleRemoveStory} /> }
     </div>
   );
 }
