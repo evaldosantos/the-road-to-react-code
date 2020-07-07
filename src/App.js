65e2796b0029aba29e65f9c7ea962adf423bbf4e
Evaldo Santos <jesantosjr@protonmail.com>
2020-07-06 21:37:16 -0300

    Inline Handler in JSX

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function getTitle(title) {
   return title;
 }
 
-const Item = ({ item }) => (
+const Item = ({ item, onRemoveItem }) => (
   <div>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -24,11 +24,16 @@ const Item = ({ item }) => (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={onRemoveItem.bind(null, item)}>
+        Dismiss
+      </button>
+    </span>
   </div>
 );
 
-const List = ({ list }) => {
-  return list.map(item => <Item key={item.objectID} item={item} />)
+const List = ({ list, onRemoveItem }) => {
+  return list.map(item => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />)
 }
 
 const InputWithLabel = ({
@@ -62,36 +67,43 @@ const InputWithLabel = ({
   );
 }
 
-function App() {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
+function App() {
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
     'search',
     'React'
   );
 
+  const [stories, setStories] = React.useState(initialStories);
+
   const handleSearch = event => {
     setSearchTerm(event.target.value);
   };
 
-  
+  const handleRemoveStory = item => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  };
 
   const searchedStories = stories.filter(function(story) {
     return story.title
@@ -111,7 +123,7 @@ function App() {
         <strong>Search</strong>
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 }
